35318c5acdbc678b8335521c9462321191543e7e
sssssLug <ssssslug@gmail.com>
2025-08-20 21:28:26 +0800

    Fix rules; Update dependency (TaCZ).

--- src/main/java/com/alrexu/parcool/compat/extern/tacz/EventHandlerForTaCZ.java
+++ src/main/java/com/alrexu/parcool/compat/extern/tacz/EventHandlerForTaCZ.java
@@ -1,22 +1,20 @@
 package com.alrexu.parcool.compat.extern.tacz;
 
-import com.alrex.parcool.api.unstable.action.ParCoolActionEvent;
 import com.alrex.parcool.api.unstable.animation.AnimationPart;
 import com.alrex.parcool.api.unstable.animation.ParCoolAnimationInfoEvent;
 import com.alrex.parcool.client.animation.Animator;
 import com.alrex.parcool.client.animation.impl.*;
-import com.alrex.parcool.common.action.Action;
 import com.alrex.parcool.common.action.impl.*;
+import com.alrex.parcool.common.capability.IStamina;
 import com.alrex.parcool.common.capability.Parkourability;
 import com.tacz.guns.api.entity.IGunOperator;
-import com.tacz.guns.api.event.common.GunFireEvent;
 import com.tacz.guns.api.event.common.GunShootEvent;
 import com.tacz.guns.api.item.gun.AbstractGunItem;
-import com.tacz.guns.resource.modifier.AttachmentCacheProperty;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventHandlerForTaCZ {
@@ -35,8 +33,8 @@ public class EventHandlerForTaCZ {
 //                    || animator instanceof SpeedVaultAnimator
 //                    || animator instanceof ChargeJumpAnimator
 //                    || animator instanceof JumpChargingAnimator
-                    || animator instanceof ClingToCliffAnimator
-//                    || animator instanceof BackwardWallJumpAnimator
+//                    || animator instanceof ClingToCliffAnimator
+                    || animator instanceof BackwardWallJumpAnimator
 //                    || animator instanceof WallJumpAnimator
                     || animator instanceof ClimbUpAnimator
                     || animator instanceof FlippingAnimator
@@ -61,11 +59,19 @@ public class EventHandlerForTaCZ {
                     || animator instanceof SpeedVaultAnimator
                     || animator instanceof ChargeJumpAnimator
                     || animator instanceof CatLeapAnimator
-                    || animator instanceof BackwardWallJumpAnimator
+//                    || animator instanceof ClingToCliffAnimator
                     || animator instanceof WallJumpAnimator
                     || animator instanceof JumpFromBarAnimator
-                    || animator instanceof RideZiplineAnimator) {
+                    || animator instanceof RideZiplineAnimator
+            ) {
                 event.getOption().cancel(AnimationPart.RIGHT_ARM);
+                return;
+            }
+
+            if (animator instanceof ClingToCliffAnimator) {
+                if (isLeftHandClinging(event.getPlayer())) {
+                    event.getOption().cancel(AnimationPart.RIGHT_ARM);
+                }
                 return;
             }
 
@@ -84,47 +90,69 @@ public class EventHandlerForTaCZ {
     }*/
 
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGH)
     public static void onGunToShoot(GunShootEvent event) {
         LivingEntity shooter = event.getShooter();
-        if(shooter instanceof Player player) {
-            Parkourability instance = Parkourability.get(player);
-            if(instance == null) return;
+        if (!(shooter instanceof Player player)) return;
 
-            ClingToCliff cliff = instance.get(ClingToCliff.class);
-            if(cliff != null && cliff.isDoing()) {
-                if(cliff.getFacingDirection() == ClingToCliff.FacingDirection.RightAgainstWall) return;
-                event.setCanceled(true);
-                return;
-            }
+        Parkourability instance = Parkourability.get(player);
+        if(instance == null) return;
 
-            if(instance.isDoingAny(
-                    BreakfallReady.class,
-                    Tap.class,
-                    HorizontalWallRun.class,
-                    VerticalWallRun.class,
-                    QuickTurn.class,
-                    RideZipline.class
-            )) return;
 
-            if(instance.isDoingAny(
-                    HangDown.class,
-                    WallJump.class,
-                    JumpFromBar.class
-            )) {
-                com.tacz.guns.resource.modifier.AttachmentCacheProperty cache = IGunOperator.fromLivingEntity(shooter).getCacheProperty();
-                if(cache != null) {
-                    float weight = (float) cache.getCache("weight_modifier");
-                    if(weight <= 6.4F) return;
-                }
-                event.setCanceled(true);
-                return;
-            }
-
-            if(instance.isDoingAny(
-                    FastRun.class) || !instance.isDoingNothing()) {
-                event.setCanceled(true);
-            }
+        ClingToCliff cCliff = instance.get(ClingToCliff.class);
+        if(cCliff != null && cCliff.isDoing()) {
+            if(cCliff.getFacingDirection() == ClingToCliff.FacingDirection.RightAgainstWall) return;
+            event.setCanceled(true);
+            return;
         }
+
+        if(instance.isDoingAny(
+                Crawl.class,
+                BreakfallReady.class,
+                Tap.class,
+                HorizontalWallRun.class
+        )) return;
+
+        if(instance.isDoingAny(
+                HangDown.class,
+                WallJump.class,
+                JumpFromBar.class,
+                RideZipline.class,
+                VerticalWallRun.class
+        )) {
+            com.tacz.guns.resource.modifier.AttachmentCacheProperty cache = IGunOperator.fromLivingEntity(shooter).getCacheProperty();
+            if(cache != null) {
+                float weight = (float) cache.getCache("weight_modifier");
+                IStamina stamina = IStamina.get(player);
+                if(weight <= 6.4F && stamina != null && !stamina.isExhausted()) {
+                    stamina.consume((int) (2 * weight));
+                    return;
+                }
+            }
+            event.setCanceled(true);
+            return;
+        }
+
+        if(instance.isDoingAny(
+                FastRun.class) || !instance.isDoingNothing()) {
+            event.setCanceled(true);
+        }
+    }
+
+
+    private static boolean isLeftHandClinging(Player player) {
+        Parkourability parkourability = Parkourability.get(player);
+        if (parkourability != null) {
+            ClingToCliff cCliff = parkourability.get(ClingToCliff.class);
+            return cCliff != null && cCliff.getFacingDirection() == ClingToCliff.FacingDirection.RightAgainstWall;
+        }
+
+        return false;
+    }
+
+
+    private static boolean isLeftHandClinging(Parkourability parkourability) {
+            ClingToCliff cCliff = parkourability.get(ClingToCliff.class);
+            return cCliff != null && cCliff.getFacingDirection() == ClingToCliff.FacingDirection.RightAgainstWall;
     }
 }
