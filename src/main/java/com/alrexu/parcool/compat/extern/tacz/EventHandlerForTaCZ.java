30d2a9688f77c4294bb48aeb7e0a07472f9ceb84
sssssLug <ssssslug@gmail.com>
2025-08-25 18:24:10 +0800

    Merge the modification from branch 1.20.1-sssssLug-patch-1 to branch 1.20.1 with minimal personal characteristics.

--- src/main/java/com/alrexu/parcool/compat/extern/tacz/EventHandlerForTaCZ.java
+++ src/main/java/com/alrexu/parcool/compat/extern/tacz/EventHandlerForTaCZ.java
@@ -1,31 +1,20 @@
 package com.alrexu.parcool.compat.extern.tacz;
 
-import com.alrex.parcool.api.unstable.action.ParCoolActionEvent;
 import com.alrex.parcool.api.unstable.animation.AnimationPart;
 import com.alrex.parcool.api.unstable.animation.ParCoolAnimationInfoEvent;
 import com.alrex.parcool.client.animation.Animator;
 import com.alrex.parcool.client.animation.impl.*;
-import com.alrex.parcool.common.action.Action;
 import com.alrex.parcool.common.action.impl.*;
-import com.alrex.parcool.common.capability.IStamina;
 import com.alrex.parcool.common.capability.Parkourability;
-import com.mojang.logging.LogUtils;
-import com.tacz.guns.api.client.gameplay.IClientPlayerGunOperator;
-import com.tacz.guns.api.entity.IGunOperator;
 import com.tacz.guns.api.event.common.GunFireEvent;
 import com.tacz.guns.api.event.common.GunShootEvent;
 import com.tacz.guns.api.item.gun.AbstractGunItem;
-import com.tacz.guns.client.gameplay.LocalPlayerDataHolder;
-import com.tacz.guns.client.gameplay.LocalPlayerReload;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import org.slf4j.Logger;
 
 public class EventHandlerForTaCZ {
     @OnlyIn(Dist.CLIENT)
@@ -58,11 +47,11 @@ public class EventHandlerForTaCZ {
             }
 
             if (animator instanceof FastRunningAnimator) {
-                AbstractClientPlayer player = event.getPlayer();
+                /*AbstractClientPlayer player = event.getPlayer();
                 if (player instanceof IClientPlayerGunOperator operator && IClientPlayerGunOperator.fromLocalPlayer((LocalPlayer) player).isAim()) {
                     event.getOption().cancelAnimation();
                     return;
-                }
+                }*/
 
                 event.getOption().cancel(AnimationPart.LEFT_LEG);
                 event.getOption().cancel(AnimationPart.RIGHT_LEG);
@@ -138,30 +127,14 @@ public class EventHandlerForTaCZ {
                 CatLeap.class,
                 BreakfallReady.class,
                 Tap.class,
-                //barely
-                HorizontalWallRun.class
-        )) return;
-
-        if(instance.isDoingAny(
                 HangDown.class,
                 WallJump.class,
                 JumpFromBar.class,
                 RideZipline.class,
                 //barely
-                VerticalWallRun.class
-        )) {
-            com.tacz.guns.resource.modifier.AttachmentCacheProperty cache = IGunOperator.fromLivingEntity(shooter).getCacheProperty();
-            if(cache != null) {
-                float weight = (float) cache.getCache("weight_modifier");
-                IStamina stamina = IStamina.get(player);
-                if(weight <= 6.4F && stamina != null && !stamina.isExhausted()) {
-                    stamina.consume((int) (2 * weight));
-                    return;
-                }
-            }
-            event.setCanceled(true);
-            return;
-        }
+                VerticalWallRun.class, HorizontalWallRun.class
+        )) return;
+
 
         if(instance.isDoingAny(
                 FastRun.class) || !instance.isDoingNothing()) {
